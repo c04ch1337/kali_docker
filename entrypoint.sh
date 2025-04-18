1c42d17522d6d0d0245b4567b97e7f738680c70f
C04CH <88205891+c04ch1337@users.noreply.github.com>
2025-04-18 09:40:56 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 service ssh start
+#service cron start
 
 echo "Container is ready. SSH is available on port 22."
+
+#echo "0 2 * * * root /root/shared/cron-daily-scan.sh" >> /etc/crontab
+#echo "30 2 * * * root /root/shared/cron-daily-poc.sh" >> /etc/crontab
+
 
 exec /bin/bash
