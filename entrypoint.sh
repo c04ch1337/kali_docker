33c5806c543dab2836f977edd16743f0a8aeaf6c
C04CH <88205891+c04ch1337@users.noreply.github.com>
2025-04-18 09:42:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 service ssh start
-#service cron start
 
 echo "Container is ready. SSH is available on port 22."
 
-#echo "0 2 * * * root /root/shared/cron-daily-scan.sh" >> /etc/crontab
-#echo "30 2 * * * root /root/shared/cron-daily-poc.sh" >> /etc/crontab
-
+# Conditionally enable cron jobs
+if [ "$ENABLE_CRON" == "true" ]; then
+    echo "0 2 * * * root /root/shared/cron-daily-scan.sh" >> /etc/crontab
+    echo "30 2 * * * root /root/shared/cron-daily-poc.sh" >> /etc/crontab
+    service cron start
+else
+    echo "Cron jobs are disabled. Set ENABLE_CRON=true to enable."
+fi
 
 exec /bin/bash
